0e61538078ab5521192beba7b5310211bf9b7750
Dome05 <68159962+Dome05@users.noreply.github.com>
2024-01-31 16:31:10 +0100

    Added methods to generate a layer

--- C/neuralnet.h
+++ C/neuralnet.h
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #ifndef NEURALNET_H
 #define NEURALNET_H
 
@@ -9,6 +10,33 @@ typedef struct {
     double layerWeights[MAX_NODES_PER_LAYER][MAX_NODES_PER_LAYER];
     double layerBias[MAX_NODES_PER_LAYER];
     double deltaLayer[MAX_NODES_PER_LAYER];
+    Layer* prevLayer;
 } Layer;
+
+Layer generateLayer(int numberOfNodes, Layer* prevLayer){
+    Layer layer;
+    layer.numberOfNodes = numberOfNodes;
+    layer.prevLayer = prevLayer;
+
+    for(int i = 0; i < numberOfNodes; i++){
+        layer.layerBias[i] = ((double)rand()) / ((double)RAND_MAX);
+        for(int j = 0; j < prevLayer->numberOfNodes; j++){
+            layer.layerWeights[j][i] = ((double)rand()) / ((double)RAND_MAX);
+        }
+    }
+
+    return layer;
+}
+
+Layer generateLayer(int numberOfNodes){
+    Layer layer;
+    layer.numberOfNodes = numberOfNodes;
+
+    for(int i = 0; i < numberOfNodes; i++){
+        layer.layerBias[i] = ((double)rand()) / ((double)RAND_MAX);
+    }
+
+    return layer;
+}
 
 #endif
